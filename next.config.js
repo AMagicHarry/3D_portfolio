9f6017b2dae87a56cb3267188e49fc8ff93f0fa5
ewan tomson <tomsonewan@gmail.com>
2024-02-19 09:07:07 -1000

    build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    // more
+};
+module.exports = nextConfig;
